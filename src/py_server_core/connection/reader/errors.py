8c6d92c496ddceb0556d735673ee67f0f39c7476
yedidya steinmetz <yedist10@gmail.com>
2026-05-19 19:07:29 +0300

    added errors handle in Reader

---
+++ src/py_server_core/connection/reader/errors.py
@@ -0,0 +1,2 @@
+class ReadError(ConnectionError):
+    pass
--- src/py_server_core/connection/reader/reader.py
+++ src/py_server_core/connection/reader/reader.py
@@ -1,6 +1,9 @@
 import asyncio
 from typing import Optional
 
+from .errors import ReadError
+
+
 class Reader:
     def __init__(self, reader: asyncio.StreamReader):
         self._reader = reader
@@ -11,7 +14,10 @@ class Reader:
         if size <= 0:
             raise ValueError("size must be positive")
 
-        data = await asyncio.wait_for(self._reader.read(size), timeout)
+        try:
+            data = await asyncio.wait_for(self._reader.read(size), timeout)
+        except Exception as exc:
+            raise ReadError(exc) from exc
 
         if data == b'':
             raise EOFError()
@@ -22,13 +28,22 @@ class Reader:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
 
-        return await asyncio.wait_for(self._reader.readexactly(size), timeout)
+        try:
+            return await asyncio.wait_for(self._reader.readexactly(size), timeout)
+        except Exception as exc:
+            raise ReadError(exc) from exc
 
     async def read_until(self, until: bytes | bytearray | memoryview, timeout: Optional[float] = None) -> bytes:
         if not isinstance(until, (bytes, bytearray, memoryview)):
             raise TypeError("until should be bytes-like object")
 
-        return await asyncio.wait_for(self._reader.readuntil(until), timeout)
+        try:
+            return await asyncio.wait_for(self._reader.readuntil(until), timeout)
+        except Exception as exc:
+            raise ReadError(exc) from exc
 
     async def read_forever(self) -> bytes:
-        return await self._reader.read(-1)
+        try:
+            return await self._reader.read(-1)
+        except Exception as exc:
+            raise ReadError(exc) from exc
